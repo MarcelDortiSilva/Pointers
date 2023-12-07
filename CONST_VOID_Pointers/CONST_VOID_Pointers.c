0d4eae9b4fdf2cca0c8d0c344ff7763b157a447e
MarcelDortiSilva <131548997+MarcelDortiSilva@users.noreply.github.com>
2023-12-07 21:02:19 +0100

    Shows how to use const
    const *void, or const*pvalue

---
+++ CONST_VOID_Pointers/CONST_VOID_Pointers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+long var1 = 9999L;
+int item;
+//int *pcount = NULL;
+//long *pvar1 = NULL ;
+//const long int *pvar1 = &var1;
+int *const pvar1 = &var1;
+pcount = &item;
+//var1=8888L;
+
+ //printf("\nAssign a value to variable: (0-9999): ");
+ //scanf("%ld", &var1);
+ //const long int *pvar1 = &var1;
+
+
+
+
+
+
+
+
+    return 0;
+}
+
